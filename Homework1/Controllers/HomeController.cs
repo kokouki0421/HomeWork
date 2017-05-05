76a40d057205c17325e8718c99879b1200c009be
kokouki <kokouki0421@gmail.com>
2017-05-05 17:29:13 +0800

    記帳本

---
+++ Homework1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Homework1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var model = new List<MyClass>();
+            Random random = new Random();
+            for (int i = 0; i <= 100; i++)
+            {
+
+                model.Add(
+                    new MyClass
+                    {
+                        category = "收入",
+                        date = DateTime.Now,
+                        money = random.Next()/1000
+                    });
+                model.Add(
+                    new MyClass
+                    {
+                        category = "支出",
+                        date = DateTime.Now,
+                        money = random.Next()/1000
+                    });
+            }
+
+
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Homework1/Models/MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework1.Models
+{
+    public class MyClass
+    {
+        public string category { get; set; }
+        public int money { get; set; }
+        public DateTime date { get; set; }
+    }
+}
